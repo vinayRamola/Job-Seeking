7528b2f63e524fb6f7f17f0a64db8f4ad6acc836
Vinay Chand Ramola <vinaychandramola123@gmail.com>
2024-07-02 22:31:49 +0530

    Deploy the backend of project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "https://job-seeking-5xut.vercel.app",
+          "https://job-seeking-5xut.vercel.app/api/v1/user",
           {
             withCredentials: true,
           }
