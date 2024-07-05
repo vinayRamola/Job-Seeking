a0aca90b6c5190249666c14a8d036fd835ea3688
Vinay Chand Ramola <vinaychandramola123@gmail.com>
2024-07-05 20:31:18 +0530

    Added backend Url

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,17 @@
 import app from "./app.js";
 import cloudinary from 'cloudinary'
 
+// Configure Cloudinary
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
     api_key: process.env.CLOUDINARY_CLIENT_API,
     api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
-  });
-  
+});
 
-app.listen(process.env.port, ()=>{
-    console.log(`Server running on port ${process.env.port}`);
-})
+// Define PORT variable
+const PORT = process.env.PORT || 4000; // Default to port 4000 if not specified in environment variables
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
