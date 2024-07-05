2be7c7c482eeed0ed6fbb74a5992559be2136698
Vinay Chand Ramola <vinaychandramola123@gmail.com>
2024-07-05 20:00:21 +0530

    Added backend Url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -42,6 +42,7 @@ const App = () => {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Home />} />
